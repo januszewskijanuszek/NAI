a0c277053dfe1f616d761274edd03e34ddf7facd
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-10-15 23:35:36 +0200

    first function update

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -10,15 +10,13 @@ using dVector = vector<double>;
 
 int main(int argc, char** argv){
     vector<string> rawArgs(argv, argc + argv);
-    dVector doubleArgv = {stod(rawArgs.at(2)), stod(rawArgs.at(3))};
+    dVector domain = {stod(rawArgs.at(2)), stod(rawArgs.at(3))};
     map<string, function<double(dVector)>> funcMap;
-
-    funcMap["bae"] = [](const vector<double>& values){
-        for(double d : values){
-            cout << d << endl;
-        }
-        return 0.0;
+    funcMap["bea"] = [](const dVector& values){
+        for(double d : values) if(-4.5 > d || d > 4.5) throw invalid_argument("Domain must be between -4.5 and 4.5");
+        return pow(1.5 - values.at(0) + values.at(0) * values.at(1), 2) +
+               pow(2.25 - values.at(0) + pow(values.at(0) * values.at(1), 2),2) +
+               pow(2.625 - values.at(0) + pow( values.at(0) * values.at(1), 3), 2);;
     };
-    funcMap[rawArgs.at(1)](doubleArgv);
     return 0;
 }
