2343733447820ef7471422690ead2bfcf171df04
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-10-16 00:12:38 +0200

    working second function update

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -25,6 +25,12 @@ int main(int argc, char** argv){
         return  (1 + pow(x + y + 1, 2) * (19 - 14 * x + 3 * pow(x, 2) - 14 * y + 6 * x * y + 3 * pow(y, 2))) *
                 (30 + pow(2 * x - 3 * y,2) * (18 - 32 * x + 12 * pow(x, 2) + 48 * y - 36 * x * y + 27 * pow(y, 2)));
     };
+    funcMap["booth"] = [](const dVector& values){
+        for(double d : values) if(-10 > d || d > 10) throw invalid_argument("Domain must be between -10 and 10");
+        double x = values.at(0);
+        double y = values.at(1);
+        return pow(x + 2 * y - 7, 2) + pow(2 * x + y - 5, 2);
+    };
     cout << funcMap[rawArgs.at(1)](domain);
     return 0;
 }
