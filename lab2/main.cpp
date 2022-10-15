715c383d5e82ca5fc6ef6ca571aab45aed43560c
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-10-15 23:41:14 +0200

    first function update

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -15,8 +15,9 @@ int main(int argc, char** argv){
     funcMap["bea"] = [](const dVector& values){
         for(double d : values) if(-4.5 > d || d > 4.5) throw invalid_argument("Domain must be between -4.5 and 4.5");
         return pow(1.5 - values.at(0) + values.at(0) * values.at(1), 2) +
-               pow(2.25 - values.at(0) + pow(values.at(0) * values.at(1), 2),2) +
-               pow(2.625 - values.at(0) + pow( values.at(0) * values.at(1), 3), 2);;
+               pow(2.25 - values.at(0) + values.at(0) * pow(values.at(1), 2),2) +
+               pow(2.625 - values.at(0) + values.at(0) *  pow(values.at(1), 3), 2);;
     };
+    cout << funcMap[rawArgs.at(1)](domain);
     return 0;
 }
