1c73626682edbfa7d030b92e0a98cdcabc10e444
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-10-15 23:17:01 +0200

    first steps

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <functional>
+#include <cmath>
+#include <string>
 
 using namespace std;
+using dVector = vector<double>;
 
-int main(){
-    cout << "Siema" << endl;
+int main(int argc, char** argv){
+    vector<string> rawArgs(argv, argc + argv);
+    dVector doubleArgv = {stod(rawArgs.at(2)), stod(rawArgs.at(3))};
+    map<string, function<double(dVector)>> funcMap;
+
+    funcMap["bae"] = [](const vector<double>& values){
+        for(double d : values){
+            cout << d << endl;
+        }
+        return 0.0;
+    };
+    funcMap[rawArgs.at(1)](doubleArgv);
     return 0;
 }
