dbc18b8ac78d845bd6d36c2f7c7f69d663a717af
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-10-15 23:54:08 +0200

    working second function update

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -18,6 +18,13 @@ int main(int argc, char** argv){
                pow(2.25 - values.at(0) + values.at(0) * pow(values.at(1), 2),2) +
                pow(2.625 - values.at(0) + values.at(0) *  pow(values.at(1), 3), 2);;
     };
+    funcMap["gold"] = [](const dVector& values){
+        for(double d : values) if(-2 > d || d > 2) throw invalid_argument("Domain must be between -2 and 2");
+        double x = values.at(0);
+        double y = values.at(1);
+        return  (1 + pow(x + y + 1, 2) * (19 - 14 * x + 3 * pow(x, 2) - 14 * y + 6 * x * y + 3 * pow(y, 2))) *
+                (30 + pow(2 * x - 3 * y,2) * (18 - 32 * x + 12 * pow(x, 2) + 48 * y - 36 * x * y + 27 * pow(y, 2)));
+    };
     cout << funcMap[rawArgs.at(1)](domain);
     return 0;
 }
