b95d8c2a7cec907c281bc28d2813f66d17d63d5f
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-11-01 21:14:04 +0100

    0.8

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -15,10 +15,11 @@ private:
     class Result{
     private:
         double time, point, x, y;
+        string type;
     public:
-        Result(double time, double point, double x, double y) : time(time), point(point), x(x), y(y) {}
+        Result(double time, double point, double x, double y, const string &type) : time(time), point(point), x(x),y(y), type(type) {}
         void writeResult(){
-            cout<<"Total Time: "<<time<<" Point: "<<point << " X -> " << x << " Y -> " << y << endl;
+            cout<< type <<" - Time: "<<time<<" Point: "<<point << " X -> " << x << " Y -> " << y << endl;
         }
     };
     class Domain{
@@ -39,6 +40,9 @@ private:
             return {x_distribution(mtGenerator), y_distribution(mtGenerator)};
         }
     };
+    function<double(vector<double>)> testFunction;
+    Domain domain;
+    string name;
     vector<double> neighborFunc(vector<double> lastPoint){
     #define RANGE 0.01
         double initialFunctionResult = testFunction(lastPoint);
@@ -52,22 +56,20 @@ private:
         uniform_real_distribution<double> distribution(range.at(0), range.at(1));
         return distribution(mtGenerator);
     }
-    function<double(vector<double>)> testFunction;
-    Domain domain;
     void hillClimbChainger(vector<double>* oldPoints, vector<double> newPoints, double* initialValue, double newInitialValue){
         *oldPoints = newPoints;
         *initialValue = newInitialValue;
     }
 public:
     TestFunction(){};
-    TestFunction(function<double(vector<double>)> testFunction, vector<double> domain){
+    TestFunction(string name, function<double(vector<double>)> testFunction, vector<double> domain){
         this -> testFunction = testFunction;
         this -> domain = Domain(domain);
+        this -> name = name;
     }
     Result bruteForce(int iteracions = 100000){
         clock_t start, end;
         start = clock();
-
         vector<double> smallestValues(2);
         double lowestPoint = numeric_limits<double>::max();
         for(int i = 0 ; i < iteracions ; i++){
@@ -79,7 +81,7 @@ public:
             }
         }
         end = clock();
-        return Result(((double)(end - start)) / CLOCKS_PER_SEC, lowestPoint, smallestValues[0], smallestValues[1]);
+        return Result(((double)(end - start)) / CLOCKS_PER_SEC, lowestPoint, smallestValues[0], smallestValues[1], name);
     }
     Result hillClimbing(int maxIterations = 10000){
     #define STEP 0.01
@@ -108,7 +110,7 @@ public:
                                   testFunction({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}));
         }
         end = clock();
-        return Result(((double)(end - start)) / CLOCKS_PER_SEC, initialValue, randomPoints.at(0), randomPoints.at(1));
+        return Result(((double)(end - start)) / CLOCKS_PER_SEC, initialValue, randomPoints.at(0), randomPoints.at(1), name);
     }
     Result anneal(int temperature = 1000) {
         #define STEP 0.01
@@ -127,7 +129,7 @@ public:
             }
         }
         end = clock();
-        return Result(((double)(end - start)) / CLOCKS_PER_SEC,testFunction(visitedPoints[1]),visitedPoints[1].at(0),visitedPoints[1].at(1));
+        return Result(((double)(end - start)) / CLOCKS_PER_SEC,testFunction(visitedPoints[1]),visitedPoints[1].at(0),visitedPoints[1].at(1), name);
     }
 };
 
@@ -137,7 +139,7 @@ int main(int argc, char** argv) {
     vector<double> domain;
     vector<TestFunction> testedFunctions(AMOUNT_OF_FUNTIONS);
     // Goldstein–Price function
-    testedFunctions[0] = TestFunction([](const vector<double>& values){
+    testedFunctions[0] = TestFunction("Goldstein-Price function",[](const vector<double>& values){
         for(double d : values) if(-2 > d || d > 2) throw invalid_argument("Input must be between -2 and 2");
         return  (1 + pow(values.at(0) + values.at(1) + 1, 2) * (19 - 14 * values.at(0) + 3 * pow(values.at(0), 2)
                  - 14 * values.at(1) + 6 * values.at(0) * values.at(1) + 3 * pow(values.at(1), 2))) *
@@ -146,14 +148,21 @@ int main(int argc, char** argv) {
                        - 36 * values.at(0) * values.at(1) + 27 * pow(values.at(1), 2)));
     }, {-2, 2, -2, 2});
     // Himmelblau's function
-    testedFunctions[1] = TestFunction([](const vector<double>& values) {
+    testedFunctions[1] = TestFunction("Himmelblau's function", [](const vector<double>& values) {
         for(double d : values) if(-5 > d || d > 5) throw invalid_argument("Input must be between -2 and 2");
         return pow(pow(values.at(0),2)+values.at(1)-11,2) + pow(values.at(0)+pow(values.at(1),2)-7,2);
     }, {-5, 5, -5, 5});
-    testedFunctions[2] = TestFunction([](const vector<double>& values){
+    // Booth function
+    testedFunctions[2] = TestFunction("Booth function",[](const vector<double>& values){
         for(double d : values) if(-10 > d || d > 10) throw invalid_argument("Input must be between -10 and 10");
         return pow(values.at(0) + 2 * values.at(1) - 7, 2) + pow(2 * values.at(0) + values.at(1) - 5, 2);
     }, {-10, 10, -10, 10});
-    testedFunctions[0].bruteForce().writeResult();
+    testedFunctions[0].anneal().writeResult();
+//    for(TestFunction testFunction : testedFunctions){
+//        cout << "---------------------------------------------" << endl;
+//        testFunction.bruteForce().writeResult();
+//        testFunction.anneal().writeResult();
+//        testFunction.hillClimbing().writeResult();
+//    }
     return 0;
 }
