08800e716879a9886fab0977eef9432325f824f0
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-11-01 17:19:55 +0100

    0.5

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -16,50 +16,50 @@ void writeResult(vector<double> result){
     cout<<"Total Time: "<<result.at(0)<<" Point: "<<result.at(1) << " X -> " << result.at(2) << " Y -> " << result.at(3) << endl;
 }
 
-void hillClimbChainger(vector<double>* oldPoints, vector<double> newPoints, double* initialValue, double newInitialValue){
-    *oldPoints = newPoints;
-    *initialValue = newInitialValue;
-}
+//void hillClimbChainger(vector<double>* oldPoints, vector<double> newPoints, double* initialValue, double newInitialValue){
+//    *oldPoints = newPoints;
+//    *initialValue = newInitialValue;
+//}
 
 // val 1 - time
 // val 2 - best result
 // val 3 - best x
 // val 4 - best y
-vector<double> hillClimbing(function<double(vector<double>)> func, vector<double> domain, int maxIterations = 10000){
-#define STEP 0.01
-    clock_t start, end;
-    start = clock();
-
-    double x_start = domain.at(0);
-    double x_end = domain.at(1);
-    double y_start = domain.at(2);
-    double y_end = domain.at(3);
-
-    uniform_real_distribution<double> x_distribution(x_start, x_end);
-    uniform_real_distribution<double> y_distribution(y_start, y_end);
-    vector<double> randomPoints = {x_distribution(mtGenerator), y_distribution(mtGenerator)};
-    vector<double> finalCoords;
-    double initialValue = func(randomPoints);
-
-    for(int i = 0 ; i < maxIterations ; i++){
-        if(randomPoints.at(0) >= x_end - STEP || randomPoints.at(0) <= x_start + STEP || randomPoints.at(1) >= y_end - STEP || randomPoints.at(1) <= y_start + STEP) break;
-        if(func({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}) < initialValue)
-            hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) + STEP},&initialValue,
-                              func({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}));
-        else if(func({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}) < initialValue)
-            hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) - STEP},&initialValue,
-                              func({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}));
-        else if(func({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}) < initialValue)
-            hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) + STEP},&initialValue,
-                              func({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}));
-        else if(func({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}) < initialValue)
-            hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) - STEP},&initialValue,
-                              func({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}));
-    }
-    end = clock();
-    double totalTime = ((double)(end - start)) / CLOCKS_PER_SEC;
-    return {totalTime, initialValue, randomPoints.at(0), randomPoints.at(1)};
-}
+//vector<double> hillClimbing(function<double(vector<double>)> func, vector<double> domain, int maxIterations = 10000){
+//#define STEP 0.01
+//    clock_t start, end;
+//    start = clock();
+//
+//    double x_start = domain.at(0);
+//    double x_end = domain.at(1);
+//    double y_start = domain.at(2);
+//    double y_end = domain.at(3);
+//
+//    uniform_real_distribution<double> x_distribution(x_start, x_end);
+//    uniform_real_distribution<double> y_distribution(y_start, y_end);
+//    vector<double> randomPoints = {x_distribution(mtGenerator), y_distribution(mtGenerator)};
+//    vector<double> finalCoords;
+//    double initialValue = func(randomPoints);
+//
+//    for(int i = 0 ; i < maxIterations ; i++){
+//        if(randomPoints.at(0) >= x_end - STEP || randomPoints.at(0) <= x_start + STEP || randomPoints.at(1) >= y_end - STEP || randomPoints.at(1) <= y_start + STEP) break;
+//        if(func({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}) < initialValue)
+//            hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) + STEP},&initialValue,
+//                              func({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}));
+//        else if(func({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}) < initialValue)
+//            hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) - STEP},&initialValue,
+//                              func({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}));
+//        else if(func({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}) < initialValue)
+//            hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) + STEP},&initialValue,
+//                              func({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}));
+//        else if(func({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}) < initialValue)
+//            hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) - STEP},&initialValue,
+//                              func({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}));
+//    }
+//    end = clock();
+//    double totalTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+//    return {totalTime, initialValue, randomPoints.at(0), randomPoints.at(1)};
+//}
 
 //void anneal(function<double(vector<double>)> function, vector<double> maxDomain, int maxIterations=1000, double step=5){
 //    uniform_real_distribution<double> dist(maxDomain.at(0), maxDomain.at(1));
@@ -112,30 +112,64 @@ class TestFunction{
 private:
     function<double(vector<double>)> testFunction;
     vector<double> maxDomain;
+    void hillClimbChainger(vector<double>* oldPoints, vector<double> newPoints, double* initialValue, double newInitialValue){
+        *oldPoints = newPoints;
+        *initialValue = newInitialValue;
+    }
 public:
-    TestFunction(const function<double(vector<double>)> &finction, const vector<double> &domain) : testFunction(finction), maxDomain(domain) {}
-    const function<double(vector<double>)> &getFinction() const {
+    TestFunction(){};
+    TestFunction(function<double(vector<double>)> testFunction, vector<double> maxDomain){
+        this -> testFunction = testFunction;
+        this -> maxDomain = maxDomain;
+    }
+    vector<double> hillClimbing(int maxIterations = 10000){
+    #define STEP 0.01
+        clock_t start, end;
+        start = clock();
+
+        double x_start = maxDomain.at(0);
+        double x_end = maxDomain.at(1);
+        double y_start = maxDomain.at(2);
+        double y_end = maxDomain.at(3);
+
+        uniform_real_distribution<double> x_distribution(x_start, x_end);
+        uniform_real_distribution<double> y_distribution(y_start, y_end);
+        vector<double> randomPoints = {x_distribution(mtGenerator), y_distribution(mtGenerator)};
+        vector<double> finalCoords;
+        double initialValue = testFunction(randomPoints);
+
+        for(int i = 0 ; i < maxIterations ; i++){
+            if(randomPoints.at(0) >= x_end - STEP || randomPoints.at(0) <= x_start + STEP || randomPoints.at(1) >= y_end - STEP || randomPoints.at(1) <= y_start + STEP) break;
+            if(testFunction({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}) < initialValue)
+                hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) + STEP},&initialValue,
+                                  testFunction({randomPoints.at(0) + STEP, randomPoints.at(1) + STEP}));
+            else if(testFunction({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}) < initialValue)
+                hillClimbChainger(&randomPoints,{randomPoints.at(0) + STEP, randomPoints.at(1) - STEP},&initialValue,
+                                  testFunction({randomPoints.at(0) + STEP, randomPoints.at(1) - STEP}));
+            else if(testFunction({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}) < initialValue)
+                hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) + STEP},&initialValue,
+                                  testFunction({randomPoints.at(0) - STEP, randomPoints.at(1) + STEP}));
+            else if(testFunction({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}) < initialValue)
+                hillClimbChainger(&randomPoints,{randomPoints.at(0) - STEP, randomPoints.at(1) - STEP},&initialValue,
+                                  testFunction({randomPoints.at(0) - STEP, randomPoints.at(1) - STEP}));
+        }
+        end = clock();
+        double totalTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+        return {totalTime, initialValue, randomPoints.at(0), randomPoints.at(1)};
+    }
+
+    const function<double(vector<double>)> &getTestFunction() const {
         return testFunction;
     }
-    void setFinction(const function<double(vector<double>)> &finction) {
-        TestFunction::testFunction = finction;
-    }
-    const vector<double> &getDomain() const {
+    const vector<double> &getMaxDomain() const {
         return maxDomain;
-    }
-    void setDomain(const vector<double> &domain) {
-        TestFunction::maxDomain = domain;
     }
 };
 
 
 int main(int argc, char** argv) {
 #define AMOUNT_OF_FUNTIONS 3
-    vector<string> input(argv, argc + argv);
     vector<double> domain;
-    // Takes 4 (double) numbers and set is as maxDomain begin and end x, y
-//    try {for (int i = 1; i < argc; i++) maxDomain.push_back(stod(input.at(i)));
-//    }catch (exception e){throw invalid_argument("Input must be a number!");}
     vector<TestFunction> testedFunctions(AMOUNT_OF_FUNTIONS);
     // Goldstein–Price function
     testedFunctions[0] = TestFunction([](const vector<double>& values){
@@ -155,9 +189,11 @@ int main(int argc, char** argv) {
         for(double d : values) if(-10 > d || d > 10) throw invalid_argument("Input must be between -10 and 10");
         return pow(values.at(0) + 2 * values.at(1) - 7, 2) + pow(2 * values.at(0) + values.at(1) - 5, 2);
     }, {-10, 10, -10, 10});
-
-    for(function<double(vector<double>)> func : funcMap){
-        cout << "siema";
+    for(int i = 0 ; i < 1000 ; i++){
+        writeResult(testedFunctions[0].hillClimbing());
     }
+//    for(TestFunction funcClass : testedFunctions){
+//        cout << "siema";
+//    }
     return 0;
 }
