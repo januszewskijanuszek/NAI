4003e42b48b84f8b17a69801563b2eda7e33abe9
januszewskijanuszek <januszewskijanuszek@gmail.com>
2022-11-01 16:53:02 +0100

    0.4

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -61,8 +61,8 @@ vector<double> hillClimbing(function<double(vector<double>)> func, vector<double
     return {totalTime, initialValue, randomPoints.at(0), randomPoints.at(1)};
 }
 
-//void anneal(function<double(vector<double>)> function, vector<double> domain, int maxIterations=1000, double step=5){
-//    uniform_real_distribution<double> dist(domain.at(0), domain.at(1));
+//void anneal(function<double(vector<double>)> function, vector<double> maxDomain, int maxIterations=1000, double step=5){
+//    uniform_real_distribution<double> dist(maxDomain.at(0), maxDomain.at(1));
 //    uniform_real_distribution<double> randomiserd(0, 3);
 //    uniform_real_distribution<double> randomiseru(0, 1);
 //    vector<double> s;
@@ -75,8 +75,8 @@ vector<double> hillClimbing(function<double(vector<double>)> func, vector<double
 //    for(int k=1;k<maxIterations;k+=step){
 //        //cout<<"\nD0"<<k<<"PA\n";
 //
-//        double a = neighbours(x,step, domain).at(randomiserd(mtGenerator));
-//        double b = neighbours(y,step, domain).at(randomiserd(mtGenerator));
+//        double a = neighbours(x,step, maxDomain).at(randomiserd(mtGenerator));
+//        double b = neighbours(y,step, maxDomain).at(randomiserd(mtGenerator));
 //        //cout<<endl<<"a:"<<a<<" b: "<<b<<endl;
 //        double tempResult = function(a,b);
 ////        cout<<s.back()<<endl;
@@ -108,33 +108,53 @@ vector<double> hillClimbing(function<double(vector<double>)> func, vector<double
 //}
 
 
+class TestFunction{
+private:
+    function<double(vector<double>)> testFunction;
+    vector<double> maxDomain;
+public:
+    TestFunction(const function<double(vector<double>)> &finction, const vector<double> &domain) : testFunction(finction), maxDomain(domain) {}
+    const function<double(vector<double>)> &getFinction() const {
+        return testFunction;
+    }
+    void setFinction(const function<double(vector<double>)> &finction) {
+        TestFunction::testFunction = finction;
+    }
+    const vector<double> &getDomain() const {
+        return maxDomain;
+    }
+    void setDomain(const vector<double> &domain) {
+        TestFunction::maxDomain = domain;
+    }
+};
+
 
 int main(int argc, char** argv) {
 #define AMOUNT_OF_FUNTIONS 3
     vector<string> input(argv, argc + argv);
     vector<double> domain;
-    // Takes 4 (double) numbers and set is as domain begin and end x, y
-//    try {for (int i = 1; i < argc; i++) domain.push_back(stod(input.at(i)));
+    // Takes 4 (double) numbers and set is as maxDomain begin and end x, y
+//    try {for (int i = 1; i < argc; i++) maxDomain.push_back(stod(input.at(i)));
 //    }catch (exception e){throw invalid_argument("Input must be a number!");}
-
-    vector<function<double(vector<double>)>> funcMap(AMOUNT_OF_FUNTIONS);
+    vector<TestFunction> testedFunctions(AMOUNT_OF_FUNTIONS);
     // Goldstein–Price function
-    funcMap[0] = [](const vector<double>& values){
+    testedFunctions[0] = TestFunction([](const vector<double>& values){
         for(double d : values) if(-2 > d || d > 2) throw invalid_argument("Input must be between -2 and 2");
         return  (1 + pow(values.at(0) + values.at(1) + 1, 2) * (19 - 14 * values.at(0) + 3 * pow(values.at(0), 2)
-                - 14 * values.at(1) + 6 * values.at(0) * values.at(1) + 3 * pow(values.at(1), 2))) *
+                 - 14 * values.at(1) + 6 * values.at(0) * values.at(1) + 3 * pow(values.at(1), 2))) *
                 (30 + pow(2 * values.at(0) - 3 * values.at(1),2) *
-                (18 - 32 * values.at(0) + 12 * pow(values.at(0), 2) + 48 * values.at(1)
-                - 36 * values.at(0) * values.at(1) + 27 * pow(values.at(1), 2)));
-    };
+                      (18 - 32 * values.at(0) + 12 * pow(values.at(0), 2) + 48 * values.at(1)
+                       - 36 * values.at(0) * values.at(1) + 27 * pow(values.at(1), 2)));
+    }, {-2, 2, -2, 2});
     // Himmelblau's function
-    funcMap[1] = [](const vector<double>& values) {
+    testedFunctions[1] = TestFunction([](const vector<double>& values) {
+        for(double d : values) if(-5 > d || d > 5) throw invalid_argument("Input must be between -2 and 2");
         return pow(pow(values.at(0),2)+values.at(1)-11,2) + pow(values.at(0)+pow(values.at(1),2)-7,2);
-    };
-    funcMap[2] = [](const vector<double>& values){
+    }, {-5, 5, -5, 5});
+    testedFunctions[2] = TestFunction([](const vector<double>& values){
         for(double d : values) if(-10 > d || d > 10) throw invalid_argument("Input must be between -10 and 10");
         return pow(values.at(0) + 2 * values.at(1) - 7, 2) + pow(2 * values.at(0) + values.at(1) - 5, 2);
-    };
+    }, {-10, 10, -10, 10});
 
     for(function<double(vector<double>)> func : funcMap){
         cout << "siema";
